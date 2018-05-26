64d619cd255c936b1d5e35f716e7b2783da34a82
RomSem <romasemenyshyn@gmail.com>
2018-05-26 10:11:49 +0300

    Added done/remove todo-tem function

---
+++ src/js/Storage.js
@@ -0,0 +1,33 @@
+export default  class Storage {
+    constructor(key) {
+        this.key = key;
+
+        if (!localStorage.getItem(key)) { // initialize todolist if it does not exist
+            localStorage.setItem(key, JSON.stringify([]));
+        }
+    }
+
+    add(item) {
+        let list = this.getAll();
+        list.push(item);
+        localStorage.setItem(this.key, JSON.stringify(list));
+    }
+
+    get(index) {
+        return this.getAll()[index];
+    }
+
+    getAll() {
+        return JSON.parse(localStorage.getItem(this.key));
+    }
+
+    remove(index) {
+        let list = this.getAll();
+        list.splice(index, 1);
+        localStorage.setItem(this.key, JSON.stringify(list));
+    }
+
+    removeAll() {
+        localStorage.removeItem(this.key);
+    }
+}
--- src/js/Todo.js
+++ src/js/Todo.js
@@ -1,54 +1,96 @@
+import Storage from './Storage'
 import checkMark from '../img/check-mark.svg';
 import crossIcon from '../img/cross.svg';
+
+
+function getElementIndex(element) {
+    let index = 0,
+        iterator = element;
+
+    while (iterator.previousElementSibling) {
+        iterator = iterator.previousElementSibling;
+        ++index;
+    }
+
+    return index;
+}
+
 
 export class Todo {
     constructor() {
         this.todoList = document.querySelector('.todo-container');
-        // this.controllBlock = this.todoList.querySelector('.control-icons');
-        // this.edit = this.controllBlock.querySelector('.todo-edit');
-        // this.toDone = this.controllBlock.querySelector('.todo-done');
-        // this.remove = this.controllBlock.querySelector('.todo-remove');
         this.letStartButton = this.todoList.querySelector('.description > .btn');
         this.TodoInput = this.todoList.querySelector('.todo-input > input');
         this.addTodoButton = this.todoList.querySelector('.todo-input > .btn');
         this.todoContent = this.todoList.querySelector('.to-dos');
+
         this.todoList = document.createElement('ul');
         this.todoContent.appendChild(this.todoList);
 
-        this.letStartButton.addEventListener('click', this.focus.bind(this));
-        this.addTodoButton.addEventListener('click', this.createTodo.bind(this, this.TodoInput));
+        this.totalTodos = document.createElement('p');
+        this.todoList.parentElement.appendChild(this.totalTodos);
 
+        this.letStartButton.addEventListener('click', () => this.TodoInput.focus());
+        this.addTodoButton.addEventListener('click', () => {
+            this._createTodoFromInput(this.TodoInput);
+        });
         this.TodoInput.addEventListener('keydown', () => {
-            if (event.keyCode !== 13) {
-                return;
+            if (event.keyCode === 13) { // esc key entered
+                this._createTodoFromInput(event.target);
             }
-
-            this.createTodo(event.target);
-
         });
 
+        this.storage = new Storage('todolist');
+        this.showTodoList();
+
+        this.todoList.addEventListener('click', () => {
+            let target = event.target,
+                doneIcon = target.closest('img.todo-done'),
+                removeIcon = target.closest('img.todo-remove'),
+                li = target.closest('.todo-item');
+
+            if (doneIcon) {
+                li.querySelector('.todo').style.textDecoration = 'line-through';
+                doneIcon.style.pointerEvents = 'none';
+
+            } else if (removeIcon) {
+                this.storage.remove(li.parentElement.children.length - 1 - getElementIndex(li));
+                li.remove();
+                this.todoCount();
+            }
+        });
+    }
+
+    _createTodoFromInput(input) {
+        const todo = {text: input.value};
+        input.value = ''; // clear input
+        this.createTodo(todo);
+        this.storage.add(todo); // save to storage
+    }
+
+    createTodo(todo) {
+        let todoItem =
+`<li class="todo-item">
+    <p class="todo">${todo.text}</p>
+    <span class="control-block">
+        <small class="todo-edit">edit</small>
+        <img class="todo-done" src="${checkMark}">
+        <img class="todo-remove" src="${crossIcon}">
+    </span>
+</li>`;
+
+        this.todoList.insertAdjacentHTML('afterBegin', todoItem);
+        this.todoCount();
+    }
+
+    todoCount() {
+        this.totalTodos.innerHTML = `Total: <span>${this.todoList.children.length}</span>`;
 
     }
 
-    focus() {
-        this.TodoInput.focus();
+    showTodoList() {
+        this.storage.getAll().forEach(todo => {
+            this.createTodo(todo);
+        });
     }
-
-
-    createTodo(input) {
-
-        let addedTodo = input.value;
-
-        if (addedTodo.length > 0) {
-            console.log(input.value);
-            let control = `<span><small>edit</small><img src="${checkMark}"><img src="${crossIcon}"></span>`;
-            let todoItem = `<li><p>${addedTodo}</p>${control}</li>`;
-
-            this.todoList.insertAdjacentHTML('afterBegin', todoItem);
-            console.log(this.todoList);
-            input.value = '';
-        }
-    }
-
-
 }
